b6add7330a1e2de763ef560d1218cb6e9934e00e
Oleg Kanatov <okanatov@gmail.com>
2015-10-27 11:39:07 +0300

    Finished example of Parser with left factoring.

--- src/main/java/org/okanatov/test/Parser.java
+++ src/main/java/org/okanatov/test/Parser.java
@@ -7,41 +7,58 @@ public class Parser {
     private final Reader reader;
     private int lookahead = -1;
 
+    private final Numbers thousands = new Thousands();
+    private final Numbers hundreds = new Hundreds();
+    private final Numbers tens = new Tens();
+    private final Numbers ones = new Ones();
+
     public Parser(Reader reader) throws IOException {
         this.reader = reader;
         this.lookahead = reader.read();
     }
 
     public int evaluate() throws IOException {
-        int result = romanNumber();
-        if (isEnd())
-            return result;
-        else
+        int thousand = romanNumber(thousands);
+        if (!isEnd(thousands))
             throw new Error("parser error");
+
+        int hundred = romanNumber(hundreds);
+        if (!isEnd(hundreds))
+            throw new Error("parser error");
+
+        int ten = romanNumber(tens);
+        if (!isEnd(tens))
+            throw new Error("parser error");
+
+        int one = romanNumber(ones);
+        if (!isEnd(ones))
+            throw new Error("parser error");
+
+        return 1000 * thousand + 100 * hundred + 10 * ten + one;
     }
 
-    private int romanNumber() throws IOException {
-        if (match('I'))
-            return 1 + romanNumber1();
-        else if (match('V'))
-            return 5 + romanNumber2() + romanNumber2() + romanNumber2();
+    private int romanNumber(Numbers numbers) throws IOException {
+        if (match(numbers.getOne()))
+            return 1 + romanNumber1(numbers);
+        else if (match(numbers.getFive()))
+            return 5 + romanNumber2(numbers) + romanNumber2(numbers) + romanNumber2(numbers);
         else
             return 0;
     }
 
-    private int romanNumber1() throws IOException {
-        if (match('I'))
-            return 1 + romanNumber2();
-        else if (match('V'))
+    private int romanNumber1(Numbers numbers) throws IOException {
+        if (match(numbers.getOne()))
+            return 1 + romanNumber2(numbers);
+        else if (match(numbers.getFive()))
             return 3;
-        else if (match('X'))
+        else if (match(numbers.getTen()))
             return 8;
         else
             return 0;
     }
 
-    private int romanNumber2() throws IOException {
-        if (match('I'))
+    private int romanNumber2(Numbers numbers) throws IOException {
+        if (match(numbers.getOne()))
             return 1;
         else
             return 0;
@@ -55,7 +72,7 @@ public class Parser {
         return false;
     }
 
-    public boolean isEnd() {
-        return lookahead == -1;
+    private boolean isEnd(Numbers numbers) {
+        return numbers.isEnd(lookahead);
     }
 }
