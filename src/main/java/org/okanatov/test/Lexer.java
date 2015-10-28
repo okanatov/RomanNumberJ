ea38dc312240fd036682e1fe18c5466b237ebaf8
Oleg Kanatov <okanatov@gmail.com>
2015-10-28 15:16:24 +0300

    First version of lexer. Works.

--- src/main/java/org/okanatov/test/Lexer.java
+++ src/main/java/org/okanatov/test/Lexer.java
@@ -2,12 +2,11 @@ package org.okanatov.test;
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.function.Predicate;
 
 public class Lexer {
-    private Reader source;
+    private Reader source = null;
+    private Lexer lexer = null;
     private String replacingString;
-    private PipedWriter pipedWriter = new PipedWriter();
     private PipedReader pipedReader;
     private PrintWriter printWriter;
     private ArrayList<String> tokens = new ArrayList<>();
@@ -15,33 +14,88 @@ public class Lexer {
     public Lexer(Reader source, String string) throws IOException {
         this.source = source;
         this.replacingString = string;
+
+        PipedWriter pipedWriter = new PipedWriter();
         pipedReader = new PipedReader(pipedWriter);
         printWriter = new PrintWriter(pipedWriter);
     }
 
-    public static Lexer createFromReader(Reader reader, String string) throws IOException {
-        return new Lexer(reader, string);
+    public Lexer(Lexer lexer, String string) throws IOException {
+        this.lexer = lexer;
+        this.replacingString = string;
+
+        PipedWriter pipedWriter = new PipedWriter();
+        pipedReader = new PipedReader(pipedWriter);
+        printWriter = new PrintWriter(pipedWriter);
     }
 
     public Reader scan() throws IOException {
-        if (tokens.size() <= 0) {
+        if (source != null)
+            return scanFromReader();
+        else
+            return scanFromLexer();
+    }
+
+    private Reader scanFromReader() throws IOException {
+        if (tokens.isEmpty()) {
             StringBuilder buffer = new StringBuilder("");
 
-            while (tokens.size() == 0) {
-                int ch;
-                if ((ch = source.read()) != -1) {
-                    buffer.append((char) ch);
+            while (tokens.isEmpty()) {
+                int ch = 0;
+                if (source.ready()) {
+                    ch = source.read();
+                    if (ch != ' ')
+                        buffer.append((char) ch);
                     if (buffer.toString().contains(replacingString)) {
                         int idx = buffer.indexOf(replacingString);
                         tokens.add(buffer.substring(0, idx));
                         tokens.add("[" + replacingString + "]");
-                        tokens.removeIf(Predicate.isEqual(""));
+                        for (String s : tokens)
+                            if (s.equals(""))
+                                tokens.remove(s);
+                        // TODO: buffer needs cleaning
+                        buffer = new StringBuilder("");
                     }
                 }
                 if (ch == -1) {
-                    tokens.add(buffer.toString());
-                    pipedWriter.close();
-                    printWriter.close();
+                    if (buffer.length() != 0)
+                        tokens.add(buffer.toString());
+                }
+            }
+        }
+        if (!tokens.isEmpty()) {
+            String result = tokens.remove(0);
+            printWriter.print(result);
+        }
+
+        return pipedReader;
+    }
+
+    private Reader scanFromLexer() throws IOException {
+        if (tokens.isEmpty()) {
+            Reader reader = lexer.scan();
+            StringBuilder buffer = new StringBuilder("");
+
+            while (tokens.isEmpty()) {
+                int ch;
+                if (reader.ready()) {
+                    ch = reader.read();
+                    if (ch != ' ')
+                        buffer.append((char) ch);
+                    if (buffer.toString().contains(replacingString)) {
+                        int idx = buffer.indexOf(replacingString);
+                        tokens.add(buffer.substring(0, idx));
+                        tokens.add("[" + replacingString + "]");
+                        for (String s : tokens)
+                            if (s.equals(""))
+                                tokens.remove(s);
+                        // TODO: buffer needs cleaning
+                        buffer = new StringBuilder("");
+                    }
+                }
+                if (!reader.ready()) {
+                    if (buffer.length() != 0)
+                        tokens.add(buffer.toString());
                 }
             }
         }
--- src/test/java/org/okanatov/test/LexerTest.java
+++ src/test/java/org/okanatov/test/LexerTest.java
@@ -12,16 +12,77 @@ public class LexerTest {
 
     @Test
     public void test4() throws IOException {
-        Lexer lexer = Lexer.createFromReader(new StringReader("IV"), "IV");
+        Lexer lexer = new Lexer(new StringReader("IV"), "IV");
         StringBuilder buffer = new StringBuilder("");
 
         int ch;
-        Reader reader;
-        while (((reader = lexer.scan()) != null) && (reader.ready())) {
+        Reader reader = lexer.scan();
+        while (reader.ready()) {
             ch = reader.read();
             buffer.append((char) ch);
         }
 
         assertEquals("[IV]", buffer.toString());
+
+        buffer = new StringBuilder("");
+        reader = lexer.scan();
+        while (reader.ready()) {
+            ch = reader.read();
+            buffer.append((char) ch);
+        }
+
+        assertEquals(Character.toString((char) -1), buffer.toString());
+
+        buffer = new StringBuilder("");
+        reader = lexer.scan();
+        while (reader.ready()) {
+            ch = reader.read();
+            buffer.append((char) ch);
+        }
+
+        assertEquals(Character.toString((char) -1), buffer.toString());
+    }
+
+    @Test
+    public void testChains() throws Exception {
+
+        Lexer lexer = new Lexer(new Lexer(new StringReader("XIV"), "IV"), "X");
+        StringBuilder buffer = new StringBuilder("");
+
+        int ch;
+        Reader reader = lexer.scan();
+        while (reader.ready()) {
+            ch = reader.read();
+            buffer.append((char) ch);
+        }
+
+        assertEquals("[X]", buffer.toString());
+
+        buffer = new StringBuilder("");
+        reader = lexer.scan();
+        while (reader.ready()) {
+            ch = reader.read();
+            buffer.append((char) ch);
+        }
+
+        assertEquals("[IV]", buffer.toString());
+
+        buffer = new StringBuilder("");
+        reader = lexer.scan();
+        while (reader.ready()) {
+            ch = reader.read();
+            buffer.append((char) ch);
+        }
+
+        assertEquals(Character.toString((char) -1), buffer.toString());
+
+        buffer = new StringBuilder("");
+        reader = lexer.scan();
+        while (reader.ready()) {
+            ch = reader.read();
+            buffer.append((char) ch);
+        }
+
+        assertEquals(Character.toString((char) -1), buffer.toString());
     }
 }
